05d8c1f183e4424f2e935bdf87d5871198136c65
Phạm Tiến Quang <quangpthe176456@fpt.edu.vn>
2025-12-04 21:09:56 +0700

    Update servlet and jsp for User List

---
+++ LaptopWMS_SWP391/src/java/Servlet/UserServlet.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Servlet;
+
+import DAO.UserDAO;
+import Model.Users;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ *
+ * @author ASUS
+ */
+@WebServlet(name = "UserServlet", urlPatterns = {"/user"})
+public class UserServlet extends HttpServlet {
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        UserDAO userDAO = new UserDAO();
+        List<Users> users = userDAO.getListUsers();
+
+        request.setAttribute("users", users);
+        request.getRequestDispatcher("/user-list.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+}
