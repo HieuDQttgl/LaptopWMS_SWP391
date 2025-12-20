27315722b2a0228235cde06d022cb460aae697b2
HieuDQttgl <electrify.maneuver@gmail.com>
2025-12-21 02:24:09 +0700

    Added Partner Model, PartnerDAO, Add, view list, view detail and change status for customer

---
+++ LaptopWMS_SWP391/src/java/DAO/PartnerDAO.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import static DAO.DBContext.getConnection;
+import Model.Partners;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.ResultSet;
+/**
+ *
+ * @author Admin
+ */
+public class PartnerDAO {
+   
+    public List<Partners> getCustomers() {
+        List<Partners> list = new ArrayList<>();
+        String sql = "SELECT * FROM partners WHERE type = 2";
+        
+        try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+            
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Partners p = new Partners();
+                p.setPartnerId(rs.getInt("partner_id"));
+                p.setType(rs.getInt("type"));
+                p.setPartnerName(rs.getString("partner_name"));
+                p.setPartnerEmail(rs.getString("partner_email"));
+                p.setPartnerPhone(rs.getString("partner_phone"));
+                p.setStatus(rs.getString("status"));
+                
+                list.add(p);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+    public Partners getPartnerById(int id) {
+        String sql = "SELECT * FROM partners WHERE partner_id = ?";
+        try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+            
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            
+            if (rs.next()) {
+                Partners p = new Partners();
+                p.setPartnerId(rs.getInt("partner_id"));
+                p.setType(rs.getInt("type"));
+                p.setPartnerName(rs.getString("partner_name"));
+                p.setPartnerEmail(rs.getString("partner_email"));
+                p.setPartnerPhone(rs.getString("partner_phone"));
+                p.setStatus(rs.getString("status"));
+                return p;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public void addPartner(Partners p) {
+        String sql = "INSERT INTO partners (type, partner_name, partner_email, partner_phone, status) VALUES (?, ?, ?, ?, ?)";
+        
+        try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+            
+            ps.setInt(1, p.getType());         
+            ps.setString(2, p.getPartnerName());
+            ps.setString(3, p.getPartnerEmail());
+            ps.setString(4, p.getPartnerPhone());
+            ps.setString(5, p.getStatus());    
+            
+            ps.executeUpdate();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void switchStatus(int id, String currentStatus) {
+        String sql = "UPDATE partners SET status = ? WHERE partner_id = ?";
+        
+  
+        String newStatus = "active".equals(currentStatus) ? "inactive" : "active";
+        
+         try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+            
+            ps.setString(1, newStatus);
+            ps.setInt(2, id);
+            
+            ps.executeUpdate();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ LaptopWMS_SWP391/src/java/Servlet/AddCustomerServlet.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Servlet;
+
+import DAO.PartnerDAO;
+import Model.Partners;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Admin
+ */
+@WebServlet(name = "AddCustomerServlet", urlPatterns = {"/add-customer"})
+public class AddCustomerServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet AddCustomerServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet AddCustomerServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("add-customer.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+  
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String phone = request.getParameter("phone");
+        
+        Partners p = new Partners();
+        p.setPartnerName(name);
+        p.setPartnerEmail(email);
+        p.setPartnerPhone(phone);
+        
+
+        p.setType(2);         
+        p.setStatus("active");  
+
+        PartnerDAO dao = new PartnerDAO();
+        dao.addPartner(p);
+ 
+        response.sendRedirect("customer-list");
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
